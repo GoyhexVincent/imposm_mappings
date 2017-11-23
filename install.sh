f052143d0b1bc0f48b64ad553944053e2cff0c6d
vincent <vincent.goyhex@mappy.com>
2017-11-23 17:42:42 +0100

    [WIP] New name for docker install procedure

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'Lets get a dockerized database:'
+docker pull mdillon/postgis
+docker run --name test-imposm-postgis -e POSTGRES_PASSWORD=postgres -d mdillon/postgis
+
+echo 'We retrieve and unpack the imposm3 tool'
+wget https://imposm.org/static/rel/imposm3-0.4.0dev-20170519-3f00374-linux-x86-64.tar.gz
+tar -zxvf imposm3-0.4.0dev-20170519-3f00374-linux-x86-64.tar.gz
+rm imposm3-0.4.0dev-20170519-3f00374-linux-x86-64.tar.gz
+
+wget http://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf
